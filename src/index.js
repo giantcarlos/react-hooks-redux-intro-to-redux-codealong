e0b6e9a75ddea662ef03227cd77053ffd17d1934
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-03-29 22:02:39 -0500

    commit tree c492d4ad5f9675c69070f322676911e0b1524b89

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 import counterReducer from "./features/counter/counterSlice.js";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = configureStore(
+    counterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+
+ReactDOM.render(
+    <Provider store={store}>
+         <App />
+    </Provider>,
+        document.getElementById("root")
+);
